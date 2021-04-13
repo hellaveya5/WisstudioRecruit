3d7f1d27281ef705112cd8483b6451a03f4d4836
98333 <983338078@qq.com>
2021-04-13 14:35:44 +0800

    a

--- com.wisstudiorecruit/src/main/java/com/wisstudio/recruit/dao/AdminDaoimpl.java
+++ com.wisstudiorecruit/src/main/java/com/wisstudio/recruit/dao/AdminDaoimpl.java
@@ -14,20 +14,20 @@ public class AdminDaoimpl implements UserDao{
     private static JDBCUtils dbUtil = new JDBCUtils();
     @Override
     public int add(User user) {
-        String sql = "insert into student value(?,?,?,?,?,?,?,?,?,?)";
+        String sql = "insert into tab_user(user_id,user_name,user_gender,user_major,user_grade,user_contactNumber," +
+                "user_choiceofdirection,user_killmastered,user_selfIntroduce) value(?,?,?,?,?,?,?,?,?,)";
         try (Connection con = dbUtil.getCon()) {
             user.getClass().getMethod("getStudentId");
             PreparedStatement pstmt = con.prepareStatement(sql);
-            pstmt.setObject(1,user.getStudentId());
-            pstmt.setObject(2,user.getPassword());
-            pstmt.setObject(3,user.getName());
-            pstmt.setObject(4,user.getGender());
+            pstmt.setObject(1,user.getId());
+            pstmt.setObject(2,user.getName());
+            pstmt.setObject(3,user.getGender());
+            pstmt.setObject(4,user.getMajor());
             pstmt.setObject(5,user.getGrade());
-            pstmt.setObject(6,user.getMajor());
-            pstmt.setObject(7,user.getContactNumber());
-            pstmt.setObject(8,user.getChoiceOfDirection());
-            pstmt.setObject(9,user.getSkillMastered());
-            pstmt.setObject(10,user.getSelfIntroduce());
+            pstmt.setObject(6,user.getContactNumber());
+            pstmt.setObject(7,user.getChoiceOfDirection());
+            pstmt.setObject(8,user.getSkillMastered());
+            pstmt.setObject(9,user.getSelfIntroduce());
             return pstmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
